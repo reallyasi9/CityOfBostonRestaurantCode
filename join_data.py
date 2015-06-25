1b103580efce72c118ceafb3fee9d69c715d985e
Phil Killewald <phillip.killewald@gmail.com>
2015-06-25 01:40:35 -0400

    Fixing join_data to do submission data as well.

--- join_data.py
+++ join_data.py
@@ -16,27 +16,34 @@ class Closest:
 
     def __init__(self, df, cols):
         self.data = df
+        self.cols = cols
         return
 
     def __call__(self, row):
         found = self.data[(self.data.restaurant_id == row.restaurant_id) & (self.data.date <= row.date)]
         if found.shape[0] == 0:
+            # FIXME Do something smarter than averaging?
             found = self.data[(self.data.restaurant_id == row.restaurant_id)][self.cols].mean()
         else:
             found = found[self.cols].sum()
-        return row.append(found)
+        # FIXME Sometimes NaNs appear if I am missing the restaurant ID.  What to do?
+        found.fillna(0, inplace=True)
+        row[self.cols] = found
+        return row
 
 
 def create_evaluation_data(business_data, checkin_data, tip_data, review_data, tip_features, review_features,
                            target_data):
     df = target_data.copy()
-    bc_df = business_data.merge(checkin_data, on='restaurant_id')
+    bc_df = business_data.join(checkin_data)
     closest_tip = Closest(tip_data, tip_features)
-    df = df.apply(closest_tip)
+    df = pd.concat([df, pd.DataFrame(np.zeros((df.shape[0], len(tip_features))), columns=tip_features)], axis=1)
+    df = df.apply(closest_tip, axis=1)
     closest_review = Closest(review_data, review_features)
-    df = df.apply(closest_review)
+    df = pd.concat([df, pd.DataFrame(np.zeros((df.shape[0], len(review_features))), columns=review_features)], axis=1)
+    df = df.apply(closest_review, axis=1)
 
-    df = df.merge(bc_df, on='restaurant_id')
+    df = df.join(bc_df, on='restaurant_id')
     return df
 
 
@@ -54,7 +61,6 @@ def main():
     feature_re = re.compile(r'^[tr]\.')
     tip_features = [col for col in tip_data.columns if feature_re.match(col) is not None]
     review_features = [col for col in review_data.columns if feature_re.match(col) is not None]
-    feature_cols = tip_features + review_features
 
     # Add restaurant IDs to everything
     id_dict = functions.build_restaurant_id_map('data/restaurant_ids_to_yelp_ids.csv')
@@ -74,17 +80,30 @@ def main():
 
     # Sum the TFIDF features for everything that comes before in time for a given tip or review
     # FIXME maybe don't drop userid?
-    tip_data.set_index('restaurant_id', inplace=True)
-    tip_data.drop(['business_id', 'user_id', 'text'], axis=1, inplace=True)
-    review_data.set_index('restaurant_id', inplace=True)
-    review_data.drop(['business_data', 'text'], axis=1, inplace=True)
+    tip_data.reset_index(inplace=True)
+    tip_data.drop(['user_id', 'text', 'business_id'], axis=1, inplace=True)
+    review_data.reset_index(inplace=True)
+    review_data.drop(['text', 'business_id'], axis=1, inplace=True)
 
     # Finally, join everything
     training_data = create_evaluation_data(business_data, checkin_data, tip_data, review_data,
                                            tip_features, review_features, training_data)
-    training_data.to_csv("processed_data/training_data.csv", index="id")
+    training_data.to_csv("processed_data/training_data.csv", index=None)
 
-    # TODO submit data and wabbit_it!
+    # And join the submission data
+    submission_data_1 = pd.read_csv('data/SubmissionFormat.csv')
+    submission_data_1.ix[:, 'date'] = submission_data_1.ix[:, 'date'].apply(functions.date_to_seconds).astype('int32')
+    submission_data_1 = create_evaluation_data(business_data, checkin_data, tip_data, review_data,
+                                               tip_features, review_features, submission_data_1)
+    submission_data_1.to_csv("processed_data/submission_data.csv", index=None)
+
+    submission_data_2 = pd.read_csv('data/PhaseIISubmissionFormat.csv')
+    submission_data_2.ix[:, 'date'] = submission_data_2.ix[:, 'date'].apply(functions.date_to_seconds).astype('int32')
+    submission_data_2 = create_evaluation_data(business_data, checkin_data, tip_data, review_data,
+                                               tip_features, review_features, submission_data_2)
+    submission_data_2.to_csv("processed_data/phase2_data.csv", index=None)
+
+    # TODO wabbit_it!
 
     return
 
---
+++ predict.py
@@ -0,0 +1,10 @@
+__author__ = 'paste'
+
+
+
+def main():
+
+    return
+
+if __name__ == "__main__":
+    main()
